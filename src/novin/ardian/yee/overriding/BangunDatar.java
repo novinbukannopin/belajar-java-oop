2698bf78a59ed3111b42642ecdce6725d2520837
novinbukannopin <68802465+novinbukannopin@users.noreply.github.com>
2022-09-28 14:10:19 +0700

    hasil kuliah pbo pertemuan 4 luring

--- src/novin/ardian/yee/overriding/BangunDatar.java
+++ src/novin/ardian/yee/overriding/BangunDatar.java
@@ -49,8 +49,7 @@ class Lingkaran extends BangunDatar {
 }
 
 class PersegiPanjang extends BangunDatar {
-    float panjang;
-    float lebar;
+    float panjang, lebar;
 
     @Override
     float luas() {
